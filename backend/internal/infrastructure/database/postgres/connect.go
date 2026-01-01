b27bb6bb29f47456b6018346ec063f2b14f14b21
Enock Yator <ekyator02@gmail.com>
2026-01-01 19:26:07 +0300

    fix: fix .gitignore

---
+++ backend/internal/infrastructure/database/postgres/connect.go
@@ -0,0 +1,21 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func Connect(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ backend/internal/infrastructure/database/postgres/migrate.go
@@ -0,0 +1,25 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(dsn, path string) error {
+	m, err := migrate.New(
+		fmt.Sprintf("file://%s", path),
+		dsn,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
