ae961b6f29e89d1cf35d98a9d6a355f44da3a8d8
Enock Yator <ekyator02@gmail.com>
2026-01-06 02:52:18 +0300

    chore: configure sqlc and go-migrate

--- backend/cmd/migrate/main.go
+++ backend/cmd/migrate/main.go
@@ -15,15 +15,7 @@ func main() {
 		log.Fatalf("failed to load config: %v", err)
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// Build Postgres DSN
-=======
-	// Build the Postgres DSN
->>>>>>> 5e02025 (chore: configure sqlc and go-migrate)
-=======
-	// Build Postgres DSN
->>>>>>> 90020b5 (chore(migration): configure mirations)
 	dbURL := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
 		cfg.DBUser,
@@ -34,8 +26,6 @@ func main() {
 		cfg.DBSSLMode,
 	)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// Path to  migrations folder
 	migrationPath := "./internal/infrastructure/database/postgres/migrations"
 
@@ -46,17 +36,3 @@ func main() {
 
 	log.Println("✅ All migrations applied successfully")
 }
-=======
-=======
-	// Path to  migrations folder
-	migrationPath := "./internal/infrastructure/database/postgres/migrations"
-
->>>>>>> 90020b5 (chore(migration): configure mirations)
-	// Run migrations
-	if err := postgres.RunMigrations(dbURL, migrationPath); err != nil {
-		log.Fatalf("migrations failed: %v", err)
-	}
-
-	log.Println("✅ All migrations applied successfully")
-}
->>>>>>> 5e02025 (chore: configure sqlc and go-migrate)
