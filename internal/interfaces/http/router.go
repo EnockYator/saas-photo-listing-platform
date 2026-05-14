287f02cc719362b02bb22053e304aaa98c91dbd0
Enock Yator <ekyator02@gmail.com>
2026-05-14 17:20:39 +0300

    add API router

--- internal/interfaces/http/router.go
+++ internal/interfaces/http/router.go
@@ -1,2 +1,29 @@
 package http
 
+import (
+	"net/http"
+
+	"github.com/EnockYator/saas-photo-listing-platform/internal/interfaces/http/handlers"
+	"github.com/EnockYator/saas-photo-listing-platform/internal/interfaces/http/middleware"
+)
+
+func NewRouter() http.Handler {
+	mux := http.NewServeMux()
+
+	// endpoints
+	mux.HandleFunc("/", handlers.RootHandler)
+	mux.HandleFunc("/health", handlers.Health)
+	mux.HandleFunc("/health/live", handlers.Live)
+	// mux.HandleFunc("/health/ready", handler.Ready(db))
+
+	// middleware chain
+	handler := middleware.RequestID(
+		middleware.Logging(
+			middleware.Recovery(
+				middleware.Timeout(mux),
+			),
+		),
+	)
+
+	return handler
+}
