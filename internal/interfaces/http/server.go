2fa145beae8fa488bc0a5823346d8c71fcbf4947
Enock Yator <ekyator02@gmail.com>
2026-05-14 17:24:36 +0300

    update server

--- internal/interfaces/http/server.go
+++ internal/interfaces/http/server.go
@@ -1,2 +1,55 @@
 package http
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/EnockYator/saas-photo-listing-platform/internal/config"
+)
+
+type Server struct {
+	cfg *config.Config
+}
+
+func NewServer(cfg *config.Config) *Server {
+	return &Server{
+		cfg: cfg,
+	}
+}
+
+func (s *Server) Start() error {
+	router := NewRouter()
+
+	server := &http.Server{
+		Addr:         ":" + s.cfg.Server.Port,
+		Handler:      router,
+		ReadTimeout:  s.cfg.Server.ReadTimeout,
+		WriteTimeout: s.cfg.Server.WriteTimeout,
+		IdleTimeout:  s.cfg.Server.IdleTimeout,
+	}
+
+
+	go func() {
+		log.Printf("server running on %s", s.cfg.Server.Port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	// graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return server.Shutdown(ctx)
+}
