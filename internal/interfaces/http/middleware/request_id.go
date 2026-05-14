a7628169a987fc7fbb89304bfab7e70ea7f0e6c0
Enock Yator <ekyator02@gmail.com>
2026-05-14 17:19:04 +0300

    feat: add middlewares for timeout, recovery, logging, and request id

--- internal/interfaces/http/middleware/request_id.go
+++ internal/interfaces/http/middleware/request_id.go
@@ -1,2 +1,23 @@
 package middleware
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type ctxKey string
+
+const RequestIDKey ctxKey = "request_id"
+
+func RequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.NewString()
+
+		ctx := context.WithValue(r.Context(), RequestIDKey, id)
+		w.Header().Set("X-Request-ID", id)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
