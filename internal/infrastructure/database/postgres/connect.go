93d760f04beff616a30a0ab5ebeec49b734d852e
Enock Yator <ekyator02@gmail.com>
2026-05-15 17:45:35 +0300

    feat: connect to postgresql database

---
+++ internal/infrastructure/database/postgres/connect.go
@@ -0,0 +1,31 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+
+	"github.com/EnockYator/saas-photo-listing-platform/internal/config"
+)
+
+func New(cfg config.DatabaseConfig) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.URL)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
