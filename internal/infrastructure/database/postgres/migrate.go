116d059bb0ccaa21bab252b12473e87e01a5ac52
Enock Yator <ekyator02@gmail.com>
2026-05-15 17:41:58 +0300

    feat: add migration add its command line arguments

--- internal/infrastructure/database/postgres/migrate.go
+++ internal/infrastructure/database/postgres/migrate.go
@@ -1,1 +1,57 @@
 package postgres
+
+import (
+	"errors"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+const migrationsPath = "file://internal/infrastructure/database/postgres/migrations"
+
+type Migrator struct {
+	m *migrate.Migrate
+}
+
+func NewMigrator(databaseURL string) (*Migrator, error) {
+	m, err := migrate.New(
+		migrationsPath,
+		databaseURL,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Migrator{
+		m: m,
+	}, nil
+}
+
+func (m *Migrator) Up() error {
+	err := m.m.Up()
+
+	if errors.Is(err, migrate.ErrNoChange) {
+		return nil
+	}
+
+	return err
+}
+
+func (m *Migrator) Down() error {
+	err := m.m.Down()
+
+	if errors.Is(err, migrate.ErrNoChange) {
+		return nil
+	}
+
+	return err
+}
+
+func (m *Migrator) Force(version int) error {
+	return m.m.Force(version)
+}
+
+func (m *Migrator) Version() (uint, bool, error) {
+	return m.m.Version()
+}
