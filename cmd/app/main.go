36d04fb871a1c390c39c7c4994abe164bba106bb
Enock Yator <ekyator02@gmail.com>
2026-05-31 19:29:26 +0300

    chore: format files

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	_ "github.com/EnockYator/saas-photo-listing-platform/internal/cli/api"
 	_ "github.com/EnockYator/saas-photo-listing-platform/internal/cli/migrate"
-	_ "github.com/EnockYator/saas-photo-listing-platform/internal/cli/worker"
 	_ "github.com/EnockYator/saas-photo-listing-platform/internal/cli/tools"
+	_ "github.com/EnockYator/saas-photo-listing-platform/internal/cli/worker"
 
 	"github.com/EnockYator/saas-photo-listing-platform/internal/cli"
 )
